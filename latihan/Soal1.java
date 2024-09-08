64a8ff5814dc2b9e2f349e2f5c69423faa1d355b
Maharayatara <2381021@unai.edu>
2024-09-08 10:36:18 +0700

    handson2

---
+++ latihan/Soal1.java
@@ -0,0 +1,16 @@
+package latihan;
+
+import java.util.Scanner;
+
+public class Soal1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukan bilangan pertama");
+        int a = input.nextInt();
+
+        System.out.println("Masukan bilangan kedua");
+        int b = input.
+
+    }
+}
